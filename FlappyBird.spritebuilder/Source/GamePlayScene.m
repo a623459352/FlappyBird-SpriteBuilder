a5f63cfda651344cd9b4bbf3fc7c88feaae544e2
Liu Yuan <a623459352@gmail.com>
2014-11-19 20:34:54 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,18 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+                 [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    
 }
 
 // put new methods here
-
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    [character flap];
+}
 @end
